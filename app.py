2e917ebef5a75e83f51220d2cab42ee2c0a42c26
Victor <victorhbsouza@outlook.com>
2024-06-05 12:40:15 -0300

    adicionando login, admpage e iniciando clientes e fornecedores

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__, static_folder='static')
-mysql = MySQL()
-
+app.config['SECRET_KEY'] = 'seu_chave_secreta_aqui'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'F@rofa123'
-app.config['MYSQL_DB'] = 'formulario_contato'
+app.config['MYSQL_PASSWORD'] = 'farofa123'
+app.config['MYSQL_DB'] = 'formulario-contato'
 
-mysql.init_app(app)
+mysql = MySQL(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
+class User(UserMixin):
+    def __init__(self, id, username, is_admin):
+        self.id = id
+        self.username = username
+        self.is_admin = is_admin
+
+@login_manager.user_loader
+def load_user(user_id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users WHERE id = %s", (user_id,))
+    user = cur.fetchone()
+    cur.close()
+    if user:
+        return User(int(user[0]), user[1], user[3])
+    return None
 
 @app.route('/')
 def index():
@@ -25,16 +43,18 @@ def Sobre():
     return render_template('Sobre.html')
 
 @app.route('/admin')
-def Admin():
+@login_required
+def admin():
+    if not current_user.is_admin:
+        return redirect(url_for('index'))
     return render_template('admin.html')
 
-
 @app.route('/contato', methods=['GET', 'POST'])
-def Contato():
+def contato():
     if request.method == 'POST':
         nome = request.form['nome']
         email = request.form['email']
-        telefone = request.form['number']
+        telefone = request.form['telefone']
         nascimento = request.form['nascimento']
         sexo = request.form['sexo']
         tipo = request.form['tipo']
@@ -53,7 +73,28 @@ def Contato():
         
     return render_template('contato.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE username = %s", (username,))
+        user = cur.fetchone()
+        cur.close()
+        if user and check_password_hash(user[2], password):
+            user_obj = User(int(user[0]), user[1], user[3])
+            login_user(user_obj)
+            return redirect(url_for('admin' if user[3] else 'index'))
+        else:
+            flash('Login não realizado. Verifique o nome de usuário e a senha.')
+    return render_template('login.html')
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port='')
+    app.run(host='0.0.0.0', debug=True)
